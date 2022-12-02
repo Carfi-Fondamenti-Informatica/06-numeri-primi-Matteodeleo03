f8b93298ae16252ddb139466f86dc6c36faa8349
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-12-02 22:27:51 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,12 +1,4 @@
-#include "lib.h"
-bool numprimo(int a, int i){
-    if(a % i == 0){
-        if(a == i){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    numprimo(a, i + 1);
-}
+#ifndef ESERCITAZIONE_6_LIB_H
+#define ESERCITAZIONE_6_LIB_H
+bool numprimo(int,int);
+#endif
