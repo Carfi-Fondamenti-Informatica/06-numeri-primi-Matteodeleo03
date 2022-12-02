85e8fe49a22caa7e3da7a1a03cb67a9e43ca668a
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-12-02 22:25:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "lib.h"
 using namespace std;
 int main(){
-  int a;
-  bool b;
-  cin >> a;
-  b=primi(a);
-  if (b==true)
-    cout << "numero primo";
-  else if(b==false)
-    cout << "numero non primo";
-  return 0;
+    int a = 0, i = 2;
+    cin >> a;
+    if(numprimo(a, i) == true){
+        cout << "numero primo";
+    }
+    else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
