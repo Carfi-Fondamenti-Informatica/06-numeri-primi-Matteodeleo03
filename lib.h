33d37086274ec567e9e18c5c19526a22744e9af6
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-12-02 22:23:17 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,12 +1,12 @@
 #include "lib.h"
-bool numprimo (int a, int i){
-  if (a % i == 0){
-    if (a == i){
-      return true;
+bool numprimo(int a, int i){
+    if(a % i == 0){
+        if(a == i){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-      return false;
-    }
-  }
-  numprimo (a, i + 1);
+    numprimo(a, i + 1);
 }
